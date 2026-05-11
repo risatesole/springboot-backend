06d3a92d1bf37d911edd3ccfdf353eb0b44360b0
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-10 22:42:36 -0400

    chore: move health controler to health module

--- src/main/java/project/app/health/HealthController.java
+++ src/main/java/project/app/health/HealthController.java
@@ -1,4 +1,4 @@
-package project.app.controller;
+package project.app.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
